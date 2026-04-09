d257f7bc352616a695f868f971d05766998bba36
Promise Udo <info.promiseudo@gmail.com>
2026-04-09 19:42:51 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,11 @@ export const metadata: Metadata = {
   icons: {
     apple: '/icons/apple-touch-icon.png',
     icon: [
+      {
+        url: '/favicon.png',
+        type: 'image/png',
+        sizes: '32x32',
+      },
       { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
